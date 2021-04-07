9bac587bcd0599d3941b8e9a5e6f62699f8a4e41
ali1stop <1stoptutorials@gmail.com>
2021-04-07 16:33:03 +0700

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,34 @@
+# 1. This is music genre and the result will be trance
+def genre(music):
+    output = music
+    return output
+
+
+music_type = "trance"
+result_music_genre = genre(music_type)
+print(result_music_genre)  # Print trance
+
+
+# 2. This is music song and the result will be "This is what it feels like"
+def song(name):
+    output = name
+    return output
+
+
+name_song = "This is what it feels like"
+result_name_song = song(name_song)
+print(result_name_song)  # Print "This is what it feels like"
+
+
+# 3. This is music genre and the result will be trance
+def duration_seconds(dsong):
+    output = dsong
+    return output
+
+
+song_duration = 428
+result_song_duration = duration_seconds(song_duration)
+print(result_song_duration)  # Print 428
+
+
+# For Booleans I am not sure how to do that
