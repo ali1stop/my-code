5c7fecd9f0f1ba36def5a0522895e65c82dc6cce
ali1stop <1stoptutorials@gmail.com>
2021-04-08 07:34:00 +0700

    Create functions2.py

---
+++ functions2.py
@@ -0,0 +1,16 @@
+def music_i_love(music):
+    output = music
+    return output
+
+
+genre = "trance"
+song = "This is what it feels like"
+duration_seconds = 428
+
+result_genre = music_i_love(genre)
+result_song = music_i_love(song)
+result_duration = music_i_love(duration_seconds)
+
+print(result_genre)  # Print trance
+print(result_song)  # Print "This is what it feels like"
+print(result_duration)  # Print 428
